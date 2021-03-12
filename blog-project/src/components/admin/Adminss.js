b9f2cdce5f60b2fcbf08f99ee5c3b51cbe4505b4
Isiaq Solebo <i.solebo32@gmail.com>
2021-03-12 09:17:55 -0600

    Updated the admin folder

--- blog-project/src/components/admin/Adminss.js
+++ blog-project/src/components/admin/Adminss.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { Admin, Resource } from 'react-admin'
 import restProvider from 'ra-data-simple-rest'
 import PostList from '../admin/PostList'
+import PostCreate from '../admin/PostCreate'
+import PostEdit from '../admin/PostEdit'
+import UserList from '../admin/UserList'
+import UserCreate from '../admin/UserCreate'
+import UserEdit from '../admin/UserEdit'
+
+
+
+
 
 const Adminss = () => {
     return <Admin dataProvider={restProvider("http://localhost:3000")}>
         <Resource
             name='posts'
             list={PostList}
+            create={PostCreate}
             edit={PostEdit}
         />
          <Resource name='users'
